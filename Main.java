1c8104189ffcb1ec324a929dd85d7aba5430c50c
Istomina Mary <marimari.st@yandex.ru>
2023-09-24 00:39:20 +1000

    задача для итоговой аттестации по Java

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Notebook> notebooks = new HashSet<Notebook>();
-        notebooks.add(new Notebook("HP ololo 41K8", 8, false, 21432.564));
-        notebooks.add(new Notebook("Lenovo 4z-SHABOOM", 64, true, 90000.1124));
+        notebooks.add(new Notebook("HP P5", 8, false, 21432.564));
+        notebooks.add(new Notebook("Lenovo 4z", 64, true, 11000.1124));
+        notebooks.add(new Notebook("Acer 55-88", 64, true, 90000.1124));
+        notebooks.add(new Notebook("Lenovo SHABOOM", 64, true, 70000.1124));
+        notebooks.add(new Notebook("HP-54-455", 64, true, 100000.1124));
+        notebooks.add(new Notebook("Acer 99-io-00", 64, false, 20000.00));
 
         OperationWithNotebook operation = new OperationWithNotebook(notebooks);
         operation.start();
